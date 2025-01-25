d33da98748b7b23873f68e9e91c5d2bda029d475
GrantsV1 <renamamiyat1@gmail.com>
2025-01-25 18:22:51 +0700

    Initial Seperation each section landing page Update content Navbar same as features.

--- resources/views/Pemeringkatan/LandingPagePemeringkatan.blade.php
+++ resources/views/Pemeringkatan/LandingPagePemeringkatan.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <link rel="stylesheet" href="{{ asset('pemeringkatan/landingpage.css') }}">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
--- resources/views/Pemeringkatan/footerpemeringkatan.blade.php
+++ resources/views/Pemeringkatan/footerpemeringkatan.blade.php
@@ -1,5 +1,7 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('pemeringkatan/footer.css') }}">
+
 <footer class="text-white footer-custom">
     <style>
         .footer-custom {
