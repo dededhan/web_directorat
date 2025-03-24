03dee171aa5f8206e628730832d35370efa81cef
Tegar127 <mtegarfitrianoj123@gmail.com>
2025-03-24 05:37:55 +0000

    fix route profil

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::get('/tupoksi', function () {
 })->name('tupoksi.tupoksi');
 
 Route::get('/profile', function () {
-    return view('profile1.profile');
+    return view('Profile1.profile');
 })->name('profile.profile');
 
 Route::get('/strukturorganisasi', function () {
