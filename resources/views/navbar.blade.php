ab4d748395df66b8511b099cd8234b4f52a93d0d
GrantsV1 <renamamiyat1@gmail.com>
2025-03-26 11:54:29 +0700

    SSO direct link

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -60,7 +60,7 @@
                 </ul>
             </li>
             <li><a href="#" class="text-white hover:text-yellow-400">Dokumen</a></li>
-            <li><a href="#" class="text-white hover:text-yellow-400">SSO</a></li>
+            <li><a href="https://sso.unj.ac.id/login" class="text-white hover:text-yellow-400">SSO</a></li>
             <li><a class="login text-white" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Masuk</a></li>
         </ul>
     </div>
