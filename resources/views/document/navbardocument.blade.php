adda5f9cc79af7125b9b8f86276223d75f06bc05
Tegar127 <mtegarfitrianoj123@gmail.com>
2025-04-13 09:34:08 +0100

    bug icon inovasi

--- resources/views/document/navbardocument.blade.php
+++ resources/views/document/navbardocument.blade.php
@@ -1,5 +1,5 @@
 
-@include('loginpopup')
+
 <!-- Desktop Navbar - Sticky -->
 <nav class="navbar hidden md:block sticky top-0 z-50 bg-[#186862] shadow-md" style="font-family: 'Arial', sans-serif;">
     <div class="container mx-auto flex items-center py-2 px-6">
