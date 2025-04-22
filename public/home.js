837131fb5be989a3f2b83d7a1609563fcd3b3fd2
Tegar127 <mtegarfitrianoj123@gmail.com>
2025-04-22 04:44:33 +0100

    update poto home

--- public/home.js
+++ public/home.js
@@ -453,8 +453,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const images = [
         "https://media.quipper.com/media/W1siZiIsIjIwMTgvMDEvMjMvMDkvNDMvMjcvYWVjNTQ1OTctOTJiNi00Y2EyLWEzZDctMGZiNTg1ZTU1MDEzLyJdLFsicCIsInRodW1iIiwiMTIwMHhcdTAwM2UiLHt9XSxbInAiLCJjb252ZXJ0IiwiLWNvbG9yc3BhY2Ugc1JHQiAtc3RyaXAiLHsiZm9ybWF0IjoianBnIn1dXQ?sha=9c61a35270604434",
         "https://edura.unj.ac.id/edura-news/wp-content/uploads/2025/01/WhatsApp-Image-2025-01-16-at-11.47.23-scaled.jpeg",
-        "/images/logos/image_corousel.jpg",
-        
+        "{{ asset('images/logos/image_corousel.jpg') }}"
     ];
     
     // Create carousel container
