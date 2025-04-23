cdfce3fa0113ea57855564d41175d8faf0ac4207
Tegar127 <mtegarfitrianoj123@gmail.com>
2025-04-23 15:24:47 +0100

    remove sosmed navbar

--- resources/views/layout/navbarpemeringkatan.blade.php
+++ resources/views/layout/navbarpemeringkatan.blade.php
@@ -1,7 +1,7 @@
 <!-- navbar.blade.php with fully click-based dropdown menus -->
 
 <!-- Social Media Bar (Original, unchanged for desktop) -->
-<div class="social-media-bar py-2 hidden md:flex">
+<!-- <div class="social-media-bar py-2 hidden md:flex">
     <div class="container mx-auto px-6 flex justify-start space-x-4">
         <a href="#" class="hover:text-yellow-500">
             <i class="fab fa-facebook-f"></i>
@@ -16,7 +16,7 @@
             <i class="fab fa-youtube"></i>
         </a>
     </div>
-</div>
+</div> -->
 
 <style>
     /* Clean dropdown styling */
