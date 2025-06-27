fb0eac7344b0581c840d5be42141adc9b8697bbe
GrantsV1 <renamamiyat1@gmail.com>
2025-06-27 13:10:16 +0700

    Revert "Init Vite"
    This reverts commit 496b97939b81ebb018f2c11c897259f2ae3378f2.

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,10 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import fs from 'fs';
-import path from 'path';
-
-const getFiles = (dir, ext, fileList = []) => {
-    if (!fs.existsSync(dir)) {
-        return fileList;
-    }
-
-    const files = fs.readdirSync(dir);
-    files.forEach(file => {
-        const filePath = path.join(dir, file);
-        if (fs.statSync(filePath).isDirectory()) {
-            getFiles(filePath, ext, fileList);
-        } else if (path.extname(file) === ext) {
-            fileList.push(filePath.replace(/\\/g, '/'));
-        }
-    });
-    return fileList;
-};
-
-const cssFiles = getFiles('resources/css', '.css');
-const jsFiles = getFiles('resources/js', '.js');
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [...cssFiles, ...jsFiles],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
