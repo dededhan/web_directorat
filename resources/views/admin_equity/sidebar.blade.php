75f71d3682bcdeef3a9425bf027c8414322d2391
GrantsV1 <renamamiyat1@gmail.com>
2025-09-20 13:54:36 +0700

    update comdev Sidebar

--- resources/views/admin_equity/sidebar.blade.php
+++ resources/views/admin_equity/sidebar.blade.php
@@ -48,10 +48,10 @@
                     </button>
                     <div x-show="dropdownOpen" x-collapse class="pt-1 pl-5">
                         <a href="{{ route('admin_equity.comdev.index') }}" class="flex items-center space-x-3 rounded-md p-2 text-sm transition-colors duration-200 {{ request()->routeIs('admin_equity.comdev.index') ? 'bg-[#0D9488] font-semibold text-white' : 'hover:bg-gray-700' }}">
-                            <i class='bx bx-list-ul text-lg'></i><span x-show="open">Daftar</span>
+                            <i class='bx bx-list-ul text-lg'></i><span x-show="open">Managemen</span>
                         </a>
                         <a href="#" class="flex items-center space-x-3 rounded-md p-2 text-sm transition-colors duration-200 hover:bg-gray-700">
-                            <i class='bx bxs-file-plus text-lg'></i><span x-show="open">Tambah Baru</span>
+                            <i class='bx bxs-file-plus text-lg'></i><span x-show="open">Laporan</span>
                         </a>
                     </div>
                 </div>
