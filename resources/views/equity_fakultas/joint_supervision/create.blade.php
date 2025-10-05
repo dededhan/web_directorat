d98ac41cdc4022e6cc934f7c9b0d68de9ab01bf1
dededhan <114045101+dededhan@users.noreply.github.com>
2025-10-06 04:31:22 +0700

    fix fakultas id in management admin

--- resources/views/equity_fakultas/joint_supervision/create.blade.php
+++ resources/views/equity_fakultas/joint_supervision/create.blade.php
@@ -11,7 +11,7 @@
             {{-- ... (Isi form sama persis seperti visiting_professor/create.blade.php) ... --}}
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="fakultas">Fakultas</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline" id="fakultas" type="text" value="{{ Auth::user()->profile->fakultas->name ?? 'Fakultas tidak ditemukan' }}" disabled>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline" id="fakultas" type="text" value="{{ Auth::user()->profile?->fakultas?->name ?? 'Fakultas tidak ditemukan' }}"  disabled>
             </div>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="nama_pengunggah">Nama Pengunggah</label>
