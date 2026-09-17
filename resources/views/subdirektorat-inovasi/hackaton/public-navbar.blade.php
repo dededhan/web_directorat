cbd78de8e5095fa446f053e3cc90281c4eaf3e5c
dededhan <deddhan2002@gmail.com>
2026-09-17 10:10:52 +0700

    feat: add FontAwesome dependency and update hackathon title casing

--- resources/views/subdirektorat-inovasi/hackaton/public-navbar.blade.php
+++ resources/views/subdirektorat-inovasi/hackaton/public-navbar.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ route('hackaton.info') }}" class="flex min-w-0 items-center gap-3 text-gray-950">
             <img src="https://upload.wikimedia.org/wikipedia/commons/4/46/Lambang_baru_UNJ.png" alt="Logo Universitas Negeri Jakarta" class="h-11 w-11 shrink-0">
             <span class="min-w-0">
-                <span class="block text-lg font-black leading-tight">Hackathon UNJ</span>
+                <span class="block text-lg font-black leading-tight">HackAthon UNJ</span>
                 <span class="block text-xs font-bold uppercase tracking-[0.14em] text-gray-600">Program inovasi</span>
             </span>
         </a>
